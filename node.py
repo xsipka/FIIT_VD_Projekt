0edffecfedc4d50c09d4c0057726dd6fde5e5050
Adam <sipka.adam@gmail.com>
2022-03-20 18:41:56 +0100

    nodes contain more stuff

--- node.py
+++ node.py
@@ -1,7 +1,11 @@
 class Node():
-    def __init__(self, id, depth):
+    def __init__(self, id, depth, gini, count, classes):
         self.id = id
         self.depth = depth
+        self.gini = gini
+        self.classes_count = count  # pocet zaznamov v danom uzle
+        self.classes = classes      # podiel zaznamov v jednotlivych triedach
+
 
     # set node as a leaf
     def set_as_leaf(self):
@@ -9,12 +13,14 @@ class Node():
         self.right_id = False
         self.leaf = True
 
+
     # set up child nodes
     def set_child_nodes(self, left, right):
         self.left_id = left
         self.right_id = right
         self.leaf = False
     
+
     # set up feature used for decision rule and its treshold
     def set_decision_rule(self, feature, threshold):
         self.feature = feature
--- tree.py
+++ tree.py
@@ -15,6 +15,7 @@ class Tree:
         self.y = y
         self.create_sklearn_tree()
         self.create_nodes()
+        self.feature_importance = self.clf.feature_importances_     # zoznam dolezitosti jednotlivych atributov
 
 
     # create sklearn tree with desired depth
@@ -25,39 +26,39 @@ class Tree:
 
     # create dictionary of nodes
     def create_nodes(self):
-        self.node_count = self.clf.tree_.node_count
-        self.left_nodes = self.clf.tree_.children_left
-        self.right_nodes = self.clf.tree_.children_right
-        self.features = self.clf.tree_.feature
-        self.thresholds = self.clf.tree_.threshold
-
-        node_depth = np.zeros(shape=self.node_count, dtype=np.int64)
-        is_leaves = np.zeros(shape=self.node_count, dtype=bool)
+        node_count = self.clf.tree_.node_count
+        left_nodes = self.clf.tree_.children_left
+        right_nodes = self.clf.tree_.children_right
+        features = self.clf.tree_.feature
+        thresholds = self.clf.tree_.threshold
+        gini = self.clf.tree_.impurity
+        samples = self.clf.tree_.value                  # podiel zaznamov v jednotlivych triedach
+        samples_count = self.clf.tree_.n_node_samples   # pocet zaznamov v danych uzloch
+        
+        node_depth = np.zeros(shape=node_count, dtype=np.int64)
+        is_leaves = np.zeros(shape=node_count, dtype=bool)
         stack = [(0, -1)]
-        nodes_dict = {}
+        self.nodes_dict = {}
 
         while len(stack) > 0:
             node_id, parent_depth = stack.pop()
             node_depth[node_id] = parent_depth + 1
-            node = Node(node_id, node_depth[node_id])
+            node = Node(node_id, node_depth[node_id], gini[node_id], samples_count[node_id], samples[node_id])
 
             # set child nodes if we have a decision node
-            if (self.left_nodes[node_id] != self.right_nodes[node_id]):
-                stack.append((self.left_nodes[node_id], parent_depth + 1))
-                stack.append((self.right_nodes[node_id], parent_depth + 1))
-                node.set_child_nodes(self.left_nodes[node_id], self.right_nodes[node_id])
-                node.set_decision_rule(self.features[node_id], self.thresholds[node_id])
+            if (left_nodes[node_id] != right_nodes[node_id]):
+                stack.append((left_nodes[node_id], parent_depth + 1))
+                stack.append((right_nodes[node_id], parent_depth + 1))
+                node.set_child_nodes(left_nodes[node_id], right_nodes[node_id])
+                node.set_decision_rule(features[node_id], thresholds[node_id])
+            
+            # set node as a leaf
             else:
                 is_leaves[node_id] = True
                 node.set_as_leaf()
             
-            #print(node.id, node.depth, node.leaf)
-            nodes_dict[node_id] = node
-    
-
-    #
-    def get_decision_path(self):
-        pass
+            #print(node.id, node.depth, node.leaf, node.gini, node.samples)
+            self.nodes_dict[node_id] = node
 
 
 
@@ -66,36 +67,5 @@ kdd99 = loader.load_dataset('file')
 kdd99 = loader.transform_cat_features(kdd99)
 x, y = loader.split_dataset(kdd99)
 
+# tree object creation
 tree = Tree(x, y, MAX_DEPTH)
-
-
-# First let's retrieve the decision path of each sample. The decision_path
-# method allows to retrieve the node indicator functions. A non zero element of
-# indicator matrix at the position (i, j) indicates that the sample i goes
-# through the node j.
-
-node_indicator = tree.clf.decision_path(x)
-leaf_id = tree.clf.apply(x)
-
-sample_id = 1
-node_index = node_indicator.indices[node_indicator.indptr[sample_id]:
-                                    node_indicator.indptr[sample_id + 1]]
-
-print('Rules used to predict sample %s: ' % sample_id)
-for node_id in node_index:
-
-    if leaf_id[sample_id] == node_id:
-        print("leaf node {} reached, no decision here".format(leaf_id[sample_id]))
-    else:
-        if (x[sample_id, tree.features[node_id]] <= tree.thresholds[node_id]):
-            threshold_sign = "<="
-        else:
-            threshold_sign = ">"
-
-        print("decision id node %s : (X[%s, %s] (= %s) %s %s)"
-              % (node_id,
-                 sample_id,
-                 tree.features[node_id],
-                 x[sample_id, tree.features[node_id]],
-                 threshold_sign,
-                 tree.thresholds[node_id]))
