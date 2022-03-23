f76b065cc4356fe66185eb61ee9be2c7a849d557
luknov <luckyonethe@gmail.com>
2022-03-23 22:40:15 +0100

    Added another node and more node info

--- tree_viz.py
+++ tree_viz.py
@@ -14,17 +14,29 @@ def setup_scene():
 
 def visualize_tree(dt):
     x, y, z, radius = [0, 0, 0, 3]
+    xoffset = 50
+    col = color.red
     for key in dt.nodes_dict.keys():
         node_id = dt.nodes_dict[key].id
         node_depth = dt.nodes_dict[key].depth
         parent_id = dt.nodes_dict[key].parent_id
         left_id = dt.nodes_dict[key].left_id
         right_id = dt.nodes_dict[key].right_id
+        samples_count = dt.nodes_dict[key].classes_count
+        #classes = dt.nodes_dict[key].classes
+        gini = dt.nodes_dict[key].gini
+        feature = dt.nodes_dict[key].feature
+        threshold = dt.nodes_dict[key].threshold
         print("Id:",  node_id, " Depth:", node_depth, " Parent Id:",  parent_id)
-        if node_depth == 0:
-            a = cylinder(pos=vector(x, y, z), axis=vector(0, 0.5, 0), radius=radius, color=color.red)
-            label(pos=a.pos, text=f'Id: {node_id}\nDepth: {node_depth}\n Parent Id: {parent_id}', color=vector(0, 0, 0),
-                  linecolor=vector(0, 0, 0), linewidth=3, border=10, yoffset=50, xoffset=50)
+
+        a = cylinder(pos=vector(x, y, z), axis=vector(0, 0.5, 0), radius=radius, color=col)
+        label(pos=a.pos, text=f'Id: {node_id}\nDepth: {node_depth}\n Parent Id: {parent_id}\n Samples count: {samples_count}\n'
+                  f'Gini: {round(gini, 3)}\n Feature: {feature} \n Threshold: {threshold}', color=vector(0, 0, 0),
+                  linecolor=vector(0, 0, 0), linewidth=3, border=10, yoffset=50, xoffset=xoffset)
+        y -= 0.5
+        radius += 1
+        xoffset -= 100
+        col = color.blue
 
         if dt.nodes_dict[key].leaf:
             print(node_id, "is a leaf")
