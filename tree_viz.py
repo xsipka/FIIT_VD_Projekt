485e4e7e17a2001160e61d92d23cdf76dfb48002
luknov <luckyonethe@gmail.com>
2022-03-24 13:49:50 +0100

    Deleted and changed some errors for presenting

--- tree_viz.py
+++ tree_viz.py
@@ -16,6 +16,7 @@ def visualize_tree(dt):
     x, y, z, radius = [0, 0, 0, 3]
     xoffset = 50
     col = color.red
+    for_testing = 0
     for key in dt.nodes_dict.keys():
         node_id = dt.nodes_dict[key].id
         node_depth = dt.nodes_dict[key].depth
@@ -25,18 +26,27 @@ def visualize_tree(dt):
         samples_count = dt.nodes_dict[key].classes_count
         #classes = dt.nodes_dict[key].classes
         gini = dt.nodes_dict[key].gini
-        feature = dt.nodes_dict[key].feature
-        threshold = dt.nodes_dict[key].threshold
+        #feature = dt.nodes_dict[key].feature
+        #threshold = dt.nodes_dict[key].threshold
         print("Id:",  node_id, " Depth:", node_depth, " Parent Id:",  parent_id)
-
-        a = cylinder(pos=vector(x, y, z), axis=vector(0, 0.5, 0), radius=radius, color=col)
-        label(pos=a.pos, text=f'Id: {node_id}\nDepth: {node_depth}\n Parent Id: {parent_id}\n Samples count: {samples_count}\n'
-                  f'Gini: {round(gini, 3)}\n Feature: {feature} \n Threshold: {threshold}', color=vector(0, 0, 0),
+        if node_depth == 0:
+            a = cylinder(pos=vector(x, y, z), axis=vector(0, 0.5, 0), radius=radius, color=col)
+            label(pos=a.pos, text=f'Id: {node_id}\nDepth: {node_depth}\n Parent Id: {parent_id}\n Samples count: {samples_count}\n'
+                  f'Gini: {round(gini, 3)}', color=vector(0, 0, 0),
                   linecolor=vector(0, 0, 0), linewidth=3, border=10, yoffset=50, xoffset=xoffset)
-        y -= 0.5
-        radius += 1
-        xoffset -= 100
-        col = color.blue
+
+        if node_depth == 1 and for_testing == 0:
+            for_testing += 1
+            y -= 0.5
+            radius += 1
+            xoffset -= 100
+            col = color.blue
+            a = cylinder(pos=vector(x, y, z), axis=vector(0, 0.5, 0), radius=radius, color=col)
+            label(pos=a.pos + vector(-3.5, 0, 0),
+                  text=f'Id: {node_id}\nDepth: {node_depth}\n Parent Id: {parent_id}\n Samples count: {samples_count}\n'
+                       f'Gini: {round(gini, 3)}', color=vector(0, 0, 0),
+                  linecolor=vector(0, 0, 0), linewidth=3, border=10, yoffset=50, xoffset=xoffset)
+
 
         if dt.nodes_dict[key].leaf:
             print(node_id, "is a leaf")
